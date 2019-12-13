4ed94f54f5d6e54fde19bb70a772c49caa7df59e
AndrezaAmorim <amrimandreza@gmail.com>
2019-12-13 18:30:19 -0300

    Criação de pastas e arquivos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Burger Queen em construção!</h1>
+      
     </div>
   );
 }
 
-export default App;
+function App2() {
+  
+  const [counter, setCounter] = useState(0)
+  
+  return (
+    <>
+      <p>{counter}</p>
+      <button onClick= {() => setCounter(counter+1)}>Contador</button>
+    </>
+  );
+}
+
+export default App2;
+
+
+
